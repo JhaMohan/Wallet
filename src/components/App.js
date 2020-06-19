701a8907fad6238e00194b8f44d6d6c8847fd601
Aditya <adityamohan1994@gmail.com>
2020-06-19 15:43:38 +0530

    logo attached and transfer event created

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
+import DaiTokenMock from '../abis/DaiTokenMock.json'
 
 class App extends Component {
+ 
+ async componentWillMount(){
+    await this.loadWeb3();
+    await this.loadBlockcchainData();
+  }
+
+  async loadWeb3() {
+
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  
+  }
+
+  async loadBlockcchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({account: accounts[0]})
+    console.log(this.state.account)
+    const networkId = await web3.eth.getId();
+    console.log(networkId)
+    const daiTokenAddress = DaiTokenMock.networks[networkId].address
+
+    const daiTokenContract = await web3.eth.Contract(DaiTokenMock.abi,daiTokenAddress)
+    this.setState({daiTokenContract: daiTokenContract})
+    const balance = await daiTokenContract.methods.balanceOf(this.state.account).call()
+    console.log(this.state.daiTokenContract)
+    this.setState({balance:web3.utils.fromWei(balance.toString(),'ether')})
+    console.log(this.state.balance)
+    const transaction = await daiTokenContract.getPastEvents('Transfer',{fromBlock:0 ,toBlock:'latest',filter:{from: this.state.account}})
+    console.log(transaction)
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      daiTokenContract: null,
+      balance: 0,
+      transactions: []
+    }
+  }
+
+
+
+
   render() {
     return (
       <div>
